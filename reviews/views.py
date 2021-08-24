065a765fa70c6c7f2b4349f8f1ff5c108c980c98
tomasz <tomekpdevelopment@gmail.com>
2021-08-24 15:17:15 +0200

    modify thank_you view function to clas based view and add context method

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -25,6 +26,10 @@ class ReviewView(View):
         "form": form
     })
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['message'] = 'This works!'
+        return context
