e2dff083b02aaaad2965031b062d367dd2e76836
tomasz <tomekpdevelopment@gmail.com>
2021-08-24 14:16:11 +0200

    change function view to class based view

--- reviews/views.py
+++ reviews/views.py
@@ -1,28 +1,30 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from .forms import ReviewForm
-from .models import Review
 
 # Create your views here.
 
-def review(request):
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data["user_name"], 
-                review_text=form.cleaned_data['review_text'], 
-                rating=form.cleaned_data['rating'])
-            review.save()
-            print(form.cleaned_data)
-            return HttpResponseRedirect("/thank-you")
-    else:
+class ReviewView(View):
+    def get(self, request):
         form = ReviewForm()
-        
-    return render(request, "reviews/review.html", {
+        return render(request, "reviews/review.html", {
         "form": form
     })
+
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect("/thank-you")
+        
+        return render(request, "reviews/review.html", {
+        "form": form
+    })
+
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
