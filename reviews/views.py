f3405d3e8fef654b6996249bbcb0979475989138
tomasz <tomekpdevelopment@gmail.com>
2021-08-30 17:24:30 +0200

    update class views to specific class views

--- reviews/views.py
+++ reviews/views.py
@@ -2,30 +2,19 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {
-        "form": form
-    })
-
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-            return HttpResponseRedirect("/thank-you")
-        
-        return render(request, "reviews/review.html", {
-        "form": form
-    })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
@@ -35,11 +24,17 @@ class ThankYouView(TemplateView):
         context['message'] = 'This works!'
         return context
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=4)
+        return data
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+        
