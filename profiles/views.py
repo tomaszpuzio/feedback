49d59e490502a9dc7eb9d1a71dfaa313569acd32
tomasz <tomekpdevelopment@gmail.com>
2021-08-31 14:35:21 +0200

    finilize file upload - add images display template

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -14,3 +15,7 @@ class CreateProfileView(CreateView):
     fields = "__all__"
     success_url = "/profiles"
 
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
